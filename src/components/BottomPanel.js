2b445eafffbb13a494972b41749487c4313a1b7a
Mohammed Anees <anees@Mohammeds-MacBook-Air.local>
2023-10-15 21:31:00 +0530

    bug fix

--- src/components/BottomPanel.js
+++ src/components/BottomPanel.js
@@ -26,19 +26,19 @@ export default function BottomPanel() {
         <p>Budget: 55,0000</p>
         <img className="h-12 w-24 mx-auto" src="/bar.png" alt=""></img>
       </div>
-      <div className="bg-rose-200 px-24 ml-12 rounded-3xl flex flex-col items-center">
+      <div className="bg-rose-200 px-24 ml-12 rounded-3xl flex flex-col items-center relative">
         <h2 className="font-semibold text-3xl mt-8">500+</h2>
         <p className="mb-4">Total Clients</p>
         <div class="flex">
-          <div class="absolute right-64 z-20">
+          <div class="absolute right-36 z-20">
             <img src="/person.jpg" alt="Person 1" class="h-14 rounded-full" />
           </div>
-          <div class="absolute right-56 z-10">
+          <div class="absolute right-28 z-10">
             <img src="/person.jpg" alt="Person 2" class="h-14 rounded-full" />
           </div>
-          <div class="absolute right-48">
-            <img src="" alt="Person 3" class="bg-black h-14 rounded-full" />
-            <p className="absolute left-6 bottom-4 text-white">+2</p>
+          <div class="absolute right-20 z-0">
+            <div alt="Person 3" class="h-14 rounded-full bg-black p-7" />
+            <p className="text-white absolute left-6 bottom-4">+2</p>
           </div>
         </div>
       </div>
