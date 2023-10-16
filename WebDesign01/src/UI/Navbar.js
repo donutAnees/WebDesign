8bfdafa41def332fb0e0bd30237a4ee7a72308d9
Mohammed Anees <anees@Mohammeds-MacBook-Air.local>
2023-10-16 09:41:59 +0530

    bug fix

--- WebDesign01/src/UI/Navbar.js
+++ WebDesign01/src/UI/Navbar.js
@@ -2,8 +2,8 @@ import Navlist from "../components/Navlist";
 
 export default function Navbar() {
   return (
-      <section className="text-white flex justify-between items-center px-10 pt-8">
-        <img className="h-14" src="/logo.png" alt="" />
+      <section className="text-white flex justify-between items-center px-6 md:px-10 pt-8">
+        <img className="h-10 md:h-14" src="/logo.png" alt="" />
         <Navlist></Navlist>
       </section>
   );
